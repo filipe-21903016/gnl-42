15d15ae824ef7684e5aa814df2ae7ad9c1c3082c
Filipe Zarco lopes de coutinho <fzarco-l@c1r5s5.42lisboa.com>
2021-11-18 17:23:49 +0000

    testing

--- get_next_line.c
+++ get_next_line.c
@@ -1,7 +1,26 @@
 # include "get_next_line.h"
 
-char *get_next_line(int fd)
+#include <limits.h>
+#include <stdio.h>
+
+char	*get_next_line(int fd)
 {
+	static char	buffer[OPEN_MAX][BUFFER_SIZE];
+	//char		*line;
+	//int			read_bytes;
+	//int			index;
 
+	if (fd < 0)
+		return (NULL);
+	read(fd, buffer[fd], BUFFER_SIZE);
+	printf("%s\n", buffer[fd]);
+	return (NULL);
+}
 
+#include <sys/stat.h>
+#include <fcntl.h>
+int main()
+{
+	int fd = open("get_next_line.h", O_RDONLY);
+	get_next_line(fd);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,5 +1,7 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define BUFFER_SIZE 256
+# define OPEN_MAX 256
 # include <stdlib.h>
 # include <unistd.h>
 
