8957fa749eea550dbe7bc90af1a54e5dc599410f
Filipe Zarco lopes de coutinho <fzarco-l@c1r22s3.42lisboa.com>
2021-11-19 20:42:11 +0000

    still trying

--- get_next_line.c
+++ get_next_line.c
@@ -1,26 +1,134 @@
 # include "get_next_line.h"
 
 #include <limits.h>
+#include <string.h>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-char	*get_next_line(int fd)
+int	ft_strlen(const char *str)
 {
-	static char	buffer[OPEN_MAX][BUFFER_SIZE];
-	//char		*line;
-	//int			read_bytes;
-	//int			index;
+	int	count;
 
-	if (fd < 0)
-		return (NULL);
-	read(fd, buffer[fd], BUFFER_SIZE);
-	printf("%s\n", buffer[fd]);
+	count = 0;
+	while (*str++)
+		count++;
+	return (count);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			return ((char *)(s + i));
+		i++;
+	}
+	if (c == '\0')
+		return ((char *)(s + i));
 	return (NULL);
 }
 
-#include <sys/stat.h>
-#include <fcntl.h>
-int main()
+char	*ft_strncpy(char *restrict dest, const char *restrict src, size_t n)
 {
+	size_t	i;
+	char	*ptr;
+
+	i = 0;
+	ptr = dest;
+	while (src[i] != '\0' && i < n)
+		*ptr++ = src[i++];
+	while (i++ < n)
+		*ptr++ = '\0';
+	return (dest);
+}
+
+void	ft_realloc(char **buf, int size)
+{
+	char	*new;
+	int		buf_len;
+
+	buf_len = ft_strlen(*buf);
+	new = malloc(sizeof(char) * (buf_len + 1 + size));
+	if (!new)
+		return ;
+	ft_strncpy(new, *buf, buf_len);
+	printf("%s\n\n%s", new, *buf);
+	free(buf);
+	buf = &new;
+}
+
+char	*extract_line(char **buffer)
+{
+	int		i;
+	char	*line;
+	char	*new_buffer;
+
+	i = 0;
+	while (*buffer[i] != '\0')
+	{
+		if (*buffer[i] == '\n') 
+		{
+			//sacar linha
+			line = malloc(sizeof(char) * i + 1);
+			ft_strncpy(line, *buffer, i + 1);
+			printf("%s\n", line);
+			//criar memoria com novo buffer avancado i
+			new_buffer = malloc(sizeof(char) * (ft_strlen(*buffer - i) + 1));
+			ft_strncpy(new_buffer, *(buffer + i + 1), ft_strlen(*buffer - i));
+			//libertar buffer antigo
+			free(*buffer);
+			//enderecar o ptr a nova memoria
+			buffer = &new_buffer;
+			return (line);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer[OPEN_MAX];
+	char		*line;
+	int			read_bytes;
+
+	if (fd < 0)
+		return (NULL);
+	read_bytes = -1;
+	if (buffer[fd] == NULL)
+	{
+		buffer[fd] = malloc(sizeof(char) * 1);
+		buffer[fd][0] = '\0';
+	}
+	while (ft_strchr(buffer[fd], '\n') == NULL && read_bytes != 0)
+	{
+		puts("caralho");
+		ft_realloc(&buffer[fd], BUFFER_SIZE);	
+		puts("saiu do caralho");
+		if (!buffer[fd])
+			return (NULL);
+		read_bytes = read(fd, buffer[fd], BUFFER_SIZE);
+		printf("%d", read_bytes);
+	}
+	line = extract_line(&buffer[fd]);
+	if (line)
+		return (line);
+	return (NULL);
+}
+
+
+int main(int ac, char **av)
+{
+	char *line;
 	int fd = open("get_next_line.h", O_RDONLY);
-	get_next_line(fd);
+
+	(void)ac;
+	av++;
+	while ((line = get_next_line(fd))!= NULL)
+		printf("%s\n", line);
+	close(fd);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,17 +1,55 @@
 #include "get_next_line.h"
 
-char	*ft_strchr(const char *s, int c)
+int	find_newline(const char **s)
 {
 	int	i;
 
 	i = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] == (char)c)
-			return ((char *)(s + i));
+		if (s[i] == '\n')
+		{
+			*s = *s + i + 1;
+			return (i);
+		}
 		i++;
 	}
-	if (c == '\0')
-		return ((char *)(s + i));
-	return (NULL);
+	return (-1);
+}
+
+
+char	*ft_strdup(const char *s)
+{
+	int		len;
+	char	*dup;
+	char	*ptr;
+
+	len = ft_strlen(s);
+	dup = (char *)malloc((len + 1) * sizeof(char));
+	if (dup == NULL)
+		return (NULL);
+	ptr = dup;
+	while (*s)
+		*ptr++ = *s++;
+	*ptr = '\0';
+	return (dup);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	true_len;
+
+	if (s == NULL)
+		return (NULL);
+	if ((unsigned int)ft_strlen(s) < start || len < 1)
+		return (ft_strdup(""));
+	true_len = len;
+	if ((size_t)ft_strlen(s) - start < len)
+		true_len = ft_strlen(s) - start;
+	substr = malloc(sizeof(char) * (true_len + 1));
+	if (!substr || !s)
+		return (NULL);
+	ft_strlcpy(substr, s + start, true_len + 1);
+	return (substr);
 }
